c9eedc13a73ec69756829883c22cf7b53fb70f77
eulerBM <eullerborgesdamotta155@gmail.com>
2023-12-29 23:33:49 -0300

    Fazendo o JS renderizar o jogo

--- game/views.py
+++ game/views.py
@@ -1,13 +1,14 @@
 from django.contrib.auth.decorators import login_required
+from django.core.serializers import serialize
 from game.models import tasks
 from django.shortcuts import render
 
 @login_required
 def playing(request):
-    task = tasks.objects.all()
+    task_user = tasks.objects.all()[:10]
 
     context = {
-        'tasks': task,
+        'task': serialize('json', task_user),
             
     }
     return render (request, 'game/playing.html', context)
--- static/js/playing.js
+++ static/js/playing.js
@@ -1,3 +1,44 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let perguntas = JSON.parse(document.getElementById('perguntas-json').textContent);
+
+    function exibirPergunta(indice) {
+        let perguntaAtual = perguntas[indice];
+        document.getElementById('pergunta-container').innerHTML = `
+
+            <div class="container">
+
+            <div id="image">
+                <img src="${perguntaAtual.fields.image}" class="d-block w-50" alt="...">
+            </div>
+            
+            <div class="mb-3">
+                <div class="input-group">
+                      <span class="input-group-text" id="basic-addon3">In english ?</span>
+                      <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3 basic-addon4">
+                      <button type="submit" class="btn btn-success" onclick="pegarTexto(${perguntaAtual.fields.name})" >Enviar</button>
+                </div>
+            </div>
+            
+        </div>
+        `;
+
+        
+    }
+
+    function proximaPergunta(proximaIndice) {
+        if (proximaIndice < perguntas.length) {
+            exibirPergunta(proximaIndice);
+        } else {
+            alert("Você respondeu todas as perguntas!");
+        }
+    }
+
+    // Iniciando com a primeira pergunta
+    exibirPergunta(0);
+});
+
+
+
 function pegarTexto(text_task) {
 
     var inputElement = document.getElementById('basic-url');
