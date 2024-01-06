0254758011b3893ecf18a3c39587c150b7921d23
eulerBM <eullerborgesdamotta155@gmail.com>
2024-01-06 09:33:54 -0300

    Pagina de amigos

--- app_main/views.py
+++ app_main/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import render
 @login_required
 def home_page(request):
     return render(request, 'home/home.html')
+
+def friend_page(request):
+    return render (request, 'friend/friend.html')
     
     
 
--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
-from app_main.views import home_page
+from app_main.views import home_page, friend_page
 
 urlpatterns = [
+    
     path('admin/', admin.site.urls),
     path('', home_page, name='home'),
+    path('friend/', friend_page, name='friend'),
+
+    #includes
     path('game/', include('game.urls')),
     path('auth/', include('authen.urls')),
 ]
