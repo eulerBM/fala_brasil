aa6102d98e714a9e7c69daf7ce1bcde9f297c859
eulerBM <eullerborgesdamotta155@gmail.com>
2024-01-08 16:04:02 -0300

    Listar jogadores

--- app_main/views.py
+++ app_main/views.py
@@ -5,7 +5,10 @@ from django.shortcuts import render
 def home_page(request):
     return render(request, 'home/home.html')
 
+@login_required
 def friend_page(request):
+    
+
     return render (request, 'friend/friend.html')
     
     
