a053b3c02284d0b5a356df70f8394467555a8339
eulerBM <eullerborgesdamotta155@gmail.com>
2024-02-02 13:37:57 -0300

    Views do pesqusia dde amigos

--- app_main/views.py
+++ app_main/views.py
@@ -1,15 +1,26 @@
 from django.contrib.auth.decorators import login_required
+from app_main.models import CustomUser
 from django.shortcuts import render
 
 @login_required
 def home_page(request):
     return render(request, 'home/home.html')
 
+
 @login_required
 def friend_page(request):
     
-    
-    return render (request, 'friend/friend.html')
+    search_query = request.GET.get('search_query')
+
+    useres_get = CustomUser.objects.filter(first_name=search_query)
+
+    context = {
+
+        'users': useres_get,
+            
+    }
+
+    return render (request, 'friend/friend.html', context)
     
     
 
