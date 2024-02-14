6ff75a0edc4525067a73b4919a6c4e5049ec4475
eulerBM <eullerborgesdamotta155@gmail.com>
2024-02-14 19:03:53 -0300

    mmiooe

--- friend/views.py
+++ friend/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from app_main.models import CustomUser
 
+
 @login_required
 def friend_page(request):
-    
+
     search_query = request.GET.get('search_query')
 
     useres_get = CustomUser.objects.filter(first_name__icontains=search_query)
@@ -12,12 +13,12 @@ def friend_page(request):
     context = {
         'users': useres_get,   
     }
-
     return render (request, 'friend/friend.html', context)
 
 @login_required
 def add_friend(request):
     pass
+
 
 @login_required
 def visualizer_friend(request):
