db9fd55bba5cf3560dbd45387742ef891c9df7d5
eulerBM <eullerborgesdamotta155@gmail.com>
2024-03-03 21:44:18 -0300

    Sistema de teste07

--- friend/views.py
+++ friend/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from app_main.models import CustomUser
 
-
 @login_required
 def friend_page(request):
 
